1224cca6317662ae3b390cc1831ceaa921b2f3da
Michal <117671671+jerik609@users.noreply.github.com>
2022-12-27 14:00:48 +0100

    stage 2 - add test stubs

---
+++ test/tracker/data/EmailAddressValidatorTest.java
@@ -0,0 +1,14 @@
+package tracker.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmailAddressValidatorTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+
+}
---
+++ test/tracker/data/StudentValidatorTest.java
@@ -0,0 +1,4 @@
+package tracker.data;
+
+public class StudentValidatorTest {
+}
