be9bf0d037554a779928711003a52dc90342ec41
Michal <117671671+jerik609@users.noreply.github.com>
2022-12-28 01:01:51 +0100

    stage 2 - probably figured out patterns

--- src/tracker/input/Input.java
+++ src/tracker/input/Input.java
@@ -1,21 +1,66 @@
 package tracker.input;
 
 import java.security.InvalidParameterException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Input {
+    public final static String INVALID_HYPHEN_APOSTROPHE_REGEX = "(?!.*[-']{2})(?!^[-'])(?!.*[-']$).*";
+    public final static String INPUT_SPLIT_PATTERN_REGEX = " ";
+    public final static String SUB_NAME_SPLIT_PATTERN_REGEX = "[-']";
+    public final static String VALID_SUB_NAME_REGEX = "[A-Z][a-z]*";
+    public final static String VALID_EMAIL_REGEX = "(\\w+\\.)*\\w+@\\w+\\.[a-z]{3}";
+
+    public static boolean validateEmail(String input) {
+        var pattern = Pattern.compile(VALID_EMAIL_REGEX);
+        return pattern.matcher(input).matches();
+    }
+
+    public static boolean validateName(List<String> split) {
+        var pattern = Pattern.compile(VALID_SUB_NAME_REGEX);
+        return IntStream.rangeClosed(0, split.size() - 1)
+                .mapToObj(split::get)
+                .filter(item -> {
+                    // split using ' and -, the sub names must be of format [A-Z][a-z]+
+                    return !Arrays.stream(item.split(SUB_NAME_SPLIT_PATTERN_REGEX))
+                            .allMatch(subItem -> {
+                                System.out.println("SubItem: " + subItem);
+                                return pattern.matcher(subItem).matches();
+                            });
+                })
+                .peek(invalid -> System.out.println("Invalid name: " + invalid))
+                .count() == 0;
+    }
 
     public static void parseStudentStr(String input) {
-        var split = input.split(" ");
-        if (split.length < 3) {
-            throw new InvalidParameterException("Input not in format <firstname> <surname> <email address>: " + input);
+
+        final var globalPattern = Pattern.compile(INVALID_HYPHEN_APOSTROPHE_REGEX);
+        if (!globalPattern.matcher(input).matches()) {
+            throw new InvalidParameterException("Invalid hyphen/apostrophe sequence: " + input);
         }
-        final var firstname = split[0];
-        final var surname = IntStream.rangeClosed(1, split.length - 2)
-                .mapToObj(i -> split[i])
-                .reduce("", (s, s2) -> s + " " + s2);
-        final var emailAddress = split[split.length - 1];
+
+        var split = Arrays.stream(input.split(INPUT_SPLIT_PATTERN_REGEX)).toList();
+
+        if (split.size() < 3) {
+            throw new InvalidParameterException("Input cannot be split into <firstname> <surname> <email address>: " + input);
+        }
+
+
+
+        // validate parts
+
+
+        validateName(split.subList(0, split.size() - 2));
+        validateEmail(split.get(split.size() - 1));
+
+        final var firstname = split.get(0).trim();
+        final var surname = split.subList(0, split.size() - 2)
+                .stream()
+                .reduce("", (s, s2) -> s + " " + s2)
+                .trim();
+        final var emailAddress = split.get(split.size() - 1).trim();
         System.out.println(firstname + "; " + surname + "; " + emailAddress);
     }
 }
--- test/tracker/input/InputTest.java
+++ test/tracker/input/InputTest.java
@@ -2,13 +2,46 @@ package tracker.input;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputTest {
-
     @Test
-    public void test() {
+    public void fullyIntegratedValidationTest() {
         final var testStr = "Jerik The Big Bad-Ass Dog jerik.the.dogg@doge.wuf";
         Input.parseStudentStr(testStr);
     }
+
+    @Test
+    public void nameTest() {
+        final List<String> nameStr = List.of("Jean-Claude", "O'Neill");
+        assertTrue(Input.validateName(nameStr));
+    }
+
+    @Test
+    public void emailTest() {
+        final var testStr = "jerik.the.dogg@doge.wuf";
+        assertTrue(Input.validateEmail(testStr));
+
+        final var badTestStr = "jerik.the.dogg@@doge.wuf";
+        assertFalse(Input.validateEmail(badTestStr));
+    }
+
+    @Test
+    public void hyphenApostropheValidationTest() {
+        final String[] testStrs = new String[]{
+                "in--between", "in''between",
+                "in-'between", "in'-between",
+                "bordering'", "bordering-",
+                "-bordering", "'bordering"
+        };
+        final var pattern = Pattern.compile(Input.INVALID_HYPHEN_APOSTROPHE_REGEX);
+        for (var item : testStrs) {
+            System.out.println("processing: " + item);
+            assertFalse(pattern.matcher(item).matches());
+        }
+    }
+
 }
